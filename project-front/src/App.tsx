68d11d2fa3a514f04c527649568e2f0c5b4a1ce0
Hedieh <heydari.hedieh2680@gmail.com>
2024-12-12 15:53:36 +0330

    add router config

--- project-front/src/App.tsx
+++ project-front/src/App.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <h1 className="text-red-600 "> hello world! </h1>;
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
